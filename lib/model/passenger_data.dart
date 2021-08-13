289ba9c4ba74e31df8f3605386a8af812a393593
zoytsa <a.baturskiy@gmail.com>
2021-08-13 10:45:52 +0600

    good

--- lib/model/passenger_data.dart
+++ lib/model/passenger_data.dart
@@ -4,7 +4,8 @@
 
 import 'dart:convert';
 
-PassengersData passengersDataFromJson(String str) => PassengersData.fromJson(json.decode(str));
+PassengersData passengersDataFromJson(String str) =>
+    PassengersData.fromJson(json.decode(str));
 
 String passengersDataToJson(PassengersData data) => json.encode(data.toJson());
 
@@ -20,48 +21,49 @@ class PassengersData {
   List<Passenger> data;
 
   factory PassengersData.fromJson(Map<String, dynamic> json) => PassengersData(
-    totalPassengers: json["totalPassengers"],
-    totalPages: json["totalPages"],
-    data: List<Passenger>.from(json["data"].map((x) => Passenger.fromJson(x))),
-  );
+        totalPassengers: json["totalPassengers"],
+        totalPages: json["totalPages"],
+        data: List<Passenger>.from(
+            json["data"].map((x) => Passenger.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "totalPassengers": totalPassengers,
-    "totalPages": totalPages,
-    "data": List<dynamic>.from(data.map((x) => x.toJson())),
-  };
+        "totalPassengers": totalPassengers,
+        "totalPages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+      };
 }
 
 class Passenger {
   Passenger({
     required this.id,
     required this.name,
-    required this.trips,
+    //  required this.trips,
     required this.airline,
     required this.v,
   });
 
   String id;
   String name;
-  int trips;
+  // int trips;
   Airline airline;
   int v;
 
   factory Passenger.fromJson(Map<String, dynamic> json) => Passenger(
-    id: json["_id"],
-    name: json["name"],
-    trips: json["trips"],
-    airline: Airline.fromJson(json["airline"]),
-    v: json["__v"],
-  );
+        id: json["_id"],
+        name: json["name"],
+        // trips: json["trips"],
+        airline: Airline.fromJson(json["airline"][0]),
+        v: json["__v"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "name": name,
-    "trips": trips,
-    "airline": airline.toJson(),
-    "__v": v,
-  };
+        "_id": id,
+        "name": name,
+        //  "trips": trips,
+        "airline": airline.toJson(),
+        "__v": v,
+      };
 }
 
 class Airline {
@@ -86,24 +88,24 @@ class Airline {
   String established;
 
   factory Airline.fromJson(Map<String, dynamic> json) => Airline(
-    id: json["id"],
-    name: json["name"],
-    country: json["country"],
-    logo: json["logo"],
-    slogan: json["slogan"],
-    headQuaters: json["head_quaters"],
-    website: json["website"],
-    established: json["established"],
-  );
+        id: json["id"],
+        name: json["name"],
+        country: json["country"],
+        logo: json["logo"],
+        slogan: json["slogan"],
+        headQuaters: json["head_quaters"],
+        website: json["website"],
+        established: json["established"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "country": country,
-    "logo": logo,
-    "slogan": slogan,
-    "head_quaters": headQuaters,
-    "website": website,
-    "established": established,
-  };
+        "id": id,
+        "name": name,
+        "country": country,
+        "logo": logo,
+        "slogan": slogan,
+        "head_quaters": headQuaters,
+        "website": website,
+        "established": established,
+      };
 }
